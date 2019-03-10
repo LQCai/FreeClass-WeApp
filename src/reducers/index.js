31666264e5c30e284d85d367bfaa7de3a6b8a56b
lqcai <1031868928@qq.com>
2019-03-10 22:11:00 +0800

    新增判断openId是都存在的状态，授权弹窗研究中

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combine, combineReducers } from 'redux';
 import counter from './counter';
 import classMenu from './classMenu';
 import judgeRole from './judgeRole';
+import openId from './openId';
 
 export default combineReducers({
     counter,
     classMenu,
     judgeRole,
+    openId,
 })

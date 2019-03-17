98d42e5bf4663632b5901c41623a26e427ce1a8b
123 <123@qq.com>
2019-03-17 11:56:56 +0800

    公告及作业静态页面完成，添加了一个组件cardContent，

---
+++ src/components/classroomTask/classroomTask.js
@@ -0,0 +1,65 @@
+import Taro from "@tarojs/taro";
+import { View, Text, OpenData } from "@tarojs/components";
+import { AtAvatar, AtButton, AtTabs, AtTabsPane } from "taro-ui";
+import "./classroomTask.scss";
+import ClassroomCard from "../classroomCard/classroomCard";
+export default class ClassroomTask extends Taro.Component {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      current: 0
+    };
+  }
+  handleClick(value) {
+    this.setState({
+      current: value
+    });
+  }
+  render() {
+    const tabList = [
+      { title: "全部" },
+      { title: "未提交" },
+      { title: "待批改" },
+      { title: "被打回" },
+      { title: "已批改" }
+    ];
+    return (
+      <AtTabs
+        current={this.state.current}
+        tabList={tabList}
+        onClick={this.handleClick.bind(this)}
+      >
+        <AtTabsPane current={this.state.current} index={0}>
+          <View className="background">
+            <ClassroomCard
+              title="做几道练习题"
+              content="一加一啥时候等于三"
+              notes="个人作业 截止时间：03月15日 16:34"
+            />
+            
+          </View>
+        </AtTabsPane>
+        <AtTabsPane current={this.state.current} index={1}>
+          <View className="background">
+            <ClassroomCard title="做几道练习题" content="一加一啥时候等于三" />
+          </View>
+        </AtTabsPane>
+        <AtTabsPane current={this.state.current} index={2}>
+          <View className="background">
+            <ClassroomCard title="做几道练习题" content="一加一啥时候等于三" />
+          </View>
+        </AtTabsPane>
+        <AtTabsPane current={this.state.current} index={3}>
+          <View className="background">
+            <ClassroomCard title="做几道练习题" content="一加一啥时候等于三" />
+          </View>
+        </AtTabsPane>
+        <AtTabsPane current={this.state.current} index={4}>
+          <View className="background">
+            <ClassroomCard title="做几道练习题" content="一加一啥时候等于三" />
+          </View>
+        </AtTabsPane>
+      </AtTabs>
+    );
+  }
+}
---
+++ src/pages/notice/notice.js
@@ -0,0 +1,15 @@
+import Taro, { render } from "@tarojs/taro";
+import { View, Text, OpenData } from "@tarojs/components";
+import { AtCard } from "taro-ui";
+import "./studentWork.scss";
+import CardContent from "../../components/cardContent/cardContent";
+
+export default class Notice extends Taro.Component {
+  render() {
+    return (
+      <View>
+        <CardContent />
+      </View>
+    );
+  }
+}

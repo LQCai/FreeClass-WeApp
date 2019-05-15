92a0899e91f7dabc94cf22685151fc3c2f2f433b
= <=>
2019-05-15 22:04:10 +0800

    修改动态列表样式

--- src/components/detectionCard/detectionCard.js
+++ src/components/detectionCard/detectionCard.js
@@ -4,27 +4,95 @@ import './detectionCard.scss';
 import { AtIcon } from 'taro-ui';
 
 export default class DetectionCard extends Taro.Component {
+
+    componentWillMount() {
+        this.setState({
+            id: this.props.id,
+            name: this.props.name,
+            content: this.props.content,
+            images: this.props.images,
+            star: this.props.star,
+            time: this.props.time,
+            commentList: this.props.commentList
+        })
+    }
+
+    componentDidShow() {
+        this.setState({
+            id: this.props.id,
+            name: this.props.name,
+            content: this.props.content,
+            images: this.props.images,
+            star: this.props.star,
+            time: this.props.time,
+            commentList: this.props.commentList
+        })
+    }
+
+    collect() {
+
+    }
+
     render() {
-        const { name, content, images, time, star, commentCount } = this.props;
+        const star1 = 1;
+        const commentList = [
+            {
+                name: '罗钦才',
+                comment: '哈哈哈！！'
+            },
+            {
+                name: '芹菜',
+                comment: '今天做了啥？'
+            },
+            {
+                name: '芹菜',
+                comment: '今天做了啥？'
+            },
+            {
+                name: '芹菜',
+                comment: '今天做了啥？'
+            }
+        ]
         return (
-            <View className='at-article'>
+            <View className='detection'>
                 <View>
                     <Text className='name'>
-                        {name}
+                        {this.state.name}
                     </Text>
                 </View>
                 <View className='content'>
-                    {content}
+                    {this.state.comment}
                 </View>
                 <View>
-                    {images.map((image, index) => (
+                    {this.state.images.map((image, index) => (
                         <Image className='list-img' key={index} src={image} />
                     ))}
                 </View>
                 <View className='other'>
                     <Text className='time-text'>
-                        {time}
+                        {this.state.time}
                     </Text>
+                    <View className='collect' onClick={this.collect}>
+                        {star1 == 1
+                            ?
+                            <AtIcon value='star-2' />
+                            :
+                            <AtIcon value='star' />
+                        }
+                    </View>
+                    <View className='comment-action'>
+                        <AtIcon value='message' />
+                    </View>
+                </View>
+                <View className='comment'>
+                    <View className='comment-list'>
+                        {commentList.map((commentInfo, index) => (
+                            <View key={index}>
+                                <Text>{commentInfo.name + ' : '} </Text>
+                                <Text>{commentInfo.comment}</Text>
+                            </View>
+                        ))}
+                    </View>
                 </View>
             </View>
         );
